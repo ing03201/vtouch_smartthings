04c7db4c8c973bbf60b4a37cedbfd2fba8562e18
김윤수 <ing03201@gmail.com>
2023-07-25 11:14:46 +0900

    add lustre library to use websocket client

---
+++ environment.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install wget -y
+wget https://github.com/SmartThingsCommunity/SmartThingsEdgeDrivers/releases/download/apiv5_48/lua_libs-api_v5_48X.tar.gz
+tar -xvf lua_libs-api_v5_48X.tar.gz -C .gitignore/
+sudo rm lua_libs-api_v5_48X.tar.gz
